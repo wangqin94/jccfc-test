a4261ec5bbddacc000ad1714ea2749de1b3ee6c0
lidy <lidy@jccfc.com>
2021-08-25 10:46:10 +0800

    1.恢复美团被覆盖脚本 2.添加百度还款恢复注释

--- Scripts/BaiDu/test_repayNotice.py
+++ Scripts/BaiDu/test_repayNotice.py
@@ -11,6 +11,7 @@ class MyTestCase(unittest.TestCase):
         """ 测试步骤 """
         # 还款通知
         # 借据号默认为空取当前用户第一笔借据号，否则取赋值借据号
+        # type额度恢复类型:1-部分还款;2-借据结清;3-放款失败;4-银行退票
         bd = Component(data=data, loan_no=None, type="2")
         bd.notice(amount="100")
 
