6b7ed284116003f09119569df724ab738c16df9a
lidy <lidy@jccfc.com>
2021-12-13 16:20:01 +0800

    1.默认执行环境

--- config/TestEnvInfo.py
+++ config/TestEnvInfo.py
@@ -10,7 +10,7 @@ EnvList = ['hsit', 'huat', 'hdev', 'hqas', 'uat', 'hpre', 'rts']
 # TEST_ENV_INFO = 'hdev'
 # TEST_ENV_INFO = 'hsit'
 # TEST_ENV_INFO = 'hpre'
-# TEST_ENV_INFO = 'hqas'
+TEST_ENV_INFO = 'hqas'
 # TEST_ENV_INFO = 'uat'
-TEST_ENV_INFO = 'rts'
+# TEST_ENV_INFO = 'rts'
 
