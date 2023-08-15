4a4abba0aa320f57c4a0ab57b6fde143479bd2d6
lidy <lidy@jccfc.com>
2023-08-15 15:07:11 +0800

    即科放款脚本优化

--- src/impl/JiKe/JiKeSynBizImpl.py
+++ src/impl/JiKe/JiKeSynBizImpl.py
@@ -5,6 +5,7 @@
 @Author  ：jccfc
 @Date    ：2022/7/13 9:51
 """
+from src.impl.public.LoanPublicBizImpl import LoanPublicBizImpl
 from utils.Apollo import *
 from src.impl.common.CheckBizImpl import CheckBizImpl
 from src.impl.JiKe.JiKeBizImpl import JiKeBizImpl
@@ -19,7 +20,7 @@ class JiKeSynBizImpl(JiKeBizImpl):
         self.jikeCheckBizImpl = JiKeCheckBizImpl(data)
 
     # 准备借款数据
-    def preLoanapply(self, bill_date=None, term=12):
+    def preLoanApply(self, bill_date=None, term=12):
         """
         多层接口业务组装关键字：放款
         @param bill_date: 首期账单日时间 格式："2021-12-12"
@@ -39,7 +40,7 @@ class JiKeSynBizImpl(JiKeBizImpl):
         thirdApplyId = json.loads(jike.credit(applyAmount=1000).get('body'))['thirdApplyId']
 
         # 数据库陈校验授信结果是否符合预期
-        status = self.CheckBizImpl.check_credit_apply_status(thirdpart_apply_id=thirdApplyId)
+        self.CheckBizImpl.check_credit_apply_status(thirdpart_apply_id=thirdApplyId)
         # 接口层校验授信结果是否符合预期
         self.jikeCheckBizImpl.jike_check_credit_apply_status(thirdApplyId)
 
@@ -52,17 +53,16 @@ class JiKeSynBizImpl(JiKeBizImpl):
         # 接口层校验授信结果是否符合预期
         self.jikeCheckBizImpl.jike_check_loan_apply_status(thirdApplyId)
 
-        # 更新资产asset_loan_invoice_info放款时间,apply_loan_date=loan_date:
-        loan_apply_info = self.MysqlBizImpl.get_loan_apply_info(thirdpart_apply_id=thirdApplyId)
-        credit_loan_invoice = self.MysqlBizImpl.get_credit_database_info('credit_loan_invoice',
-                                                                         loan_apply_id=loan_apply_info['loan_apply_id'])
-        loan_date = str(loanDate).replace("-", '')
-        self.MysqlBizImpl.update_asset_database_info('asset_loan_invoice_info', attr="loan_invoice_id='{}'".format(
-            credit_loan_invoice['loan_invoice_id']), apply_loan_date=loan_date)
+        # 更新放款时间
+        loanPublicBizImpl = LoanPublicBizImpl()
+        loanPublicBizImpl.updateLoanInfo(thirdLoanId=thirdApplyId, loanDate=loanDate)
+
+        # 关闭放款mock
+        loanPublicBizImpl.updateLoanDateMock(flag=False)
 
         return bill_date
 
 
 if __name__ == "__main__":
     JiKeBizImpl = JiKeSynBizImpl()
-    JiKeBizImpl.preLoanapply(month=3)
+    JiKeBizImpl.preLoanApply(bill_date='2023-01-01')
