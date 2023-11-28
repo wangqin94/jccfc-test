4931b9bf30f2637bf4f130dabddd24f17ce0d538
wangpx <wangpx@jccfc.com>
2023-11-28 09:32:43 +0800

    修改环境配置

--- config/TestEnvInfo.py
+++ config/TestEnvInfo.py
@@ -11,10 +11,8 @@ credit = ['uat', 'rts', 'dev', 'sit', 'year']
 # 测试环境列表
 EnvList = ['hsit', 'huat', 'hdev', 'hqas', 'uat', 'hpre', 'dev', 'rts', 'year', 'hpet', 'hyear']
 # TEST_ENV_INFO = 'huat'
-EnvList = ['hsit', 'huat', 'hdev', 'hqas', 'uat', 'hpre', 'dev', 'rts', 'year']
-TEST_ENV_INFO = 'huat'
 # TEST_ENV_INFO = 'hdev'
-# TEST_ENV_INFO = 'hsit'
+TEST_ENV_INFO = 'hsit'
 # TEST_ENV_INFO = 'hpre'
 # TEST_ENV_INFO = 'hqas'
 # TEST_ENV_INFO = 'uat'
